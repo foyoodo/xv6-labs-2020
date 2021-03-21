f70996c07acb62e7493ca1f68476a038889da936
foyoodo <foyoodo@gmail.com>
2021-03-21 17:45:54 +0800

    Lab traps: backtrace

--- kernel/printf.c
+++ kernel/printf.c
@@ -132,3 +132,18 @@ printfinit(void)
   initlock(&pr.lock, "pr");
   pr.locking = 1;
 }
+
+void
+backtrace(void)
+{
+  printf("backtrace:\n");
+
+  uint64 fp = r_fp();
+  uint64 bottom = PGROUNDUP(fp);
+
+  while (fp < bottom) {
+    uint64 ra = *(uint64*)(fp - 8);
+    printf("%p\n", ra);
+    fp = *(uint64*)(fp - 16);
+  }
+}
--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -70,6 +70,9 @@ sys_sleep(void)
     sleep(&ticks, &tickslock);
   }
   release(&tickslock);
+
+  backtrace();
+
   return 0;
 }
 
